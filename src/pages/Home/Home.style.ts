369c8add560abaf3039fb620984e0d8dcbbaca64
imhwarang <imhwarang4@naver.com>
2024-08-31 18:15:28 +0900

    feat: home페이지 퍼블리싱 완료 및 order-info 페이지로 라우팅 연결

--- src/pages/Home/Home.style.ts
+++ src/pages/Home/Home.style.ts
@@ -1,6 +1,20 @@
 import { css, Theme } from "@emotion/react";
+import { flexGenerator } from "@styles/generator";
+
+export const layoutStyle = css`
+  ${flexGenerator("column")}
+  padding: 10rem 2rem 3rem;
+`;
 
 export const titleStyle = (theme: Theme) => css`
+  width: 100%;
   color: ${theme.color.black};
   ${theme.font["head01-b-24"]}
+`;
+
+export const chracterDivStyle = css`
+  width: 30rem;
+  height: 30rem;
+  margin-top: 2.5rem;
+  margin-bottom: 10.6rem;
 `;
