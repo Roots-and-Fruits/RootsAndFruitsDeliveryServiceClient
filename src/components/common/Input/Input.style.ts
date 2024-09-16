8f046e5da55c5fa2475f81aeacf95531395db99a
imhwarang <imhwarang4@naver.com>
2024-09-16 17:05:46 +0900

    feat: Input 컴포넌트 구현

---
+++ src/components/common/Input/Input.style.ts
@@ -0,0 +1,31 @@
+import { Theme } from "@emotion/react";
+import { css } from "@emotion/react";
+import { flexGenerator } from "@styles/generator";
+
+export const inputWrapper = css`
+  ${flexGenerator("column", "flex-start", "flex-start")}
+  gap: 1rem;
+  width: 100%;
+`;
+
+export const labelStyle = (theme: Theme) => css`
+  color: ${theme.color.black};
+  ${theme.font["subhead-n-16"]}
+`;
+
+export const inputStyle = (theme: Theme) => css`
+  width: 100%;
+  padding: 1.5rem;
+  border: 1px solid ${theme.color.lightgray1};
+  border-radius: 10px;
+  background-color: ${theme.color.white};
+
+  &::placeholder {
+    color: ${theme.color.midgray1};
+  }
+
+  &:focus {
+    outline: none;
+    border: 1px solid ${theme.color.orange};
+  }
+`;
