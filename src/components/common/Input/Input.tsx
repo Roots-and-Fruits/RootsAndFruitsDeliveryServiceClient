e580a10cf8fb51d5a96ca6e6cbe250b09a84845b
thisishwarang <101498590+thisishwarang@users.noreply.github.com>
2024-09-16 17:07:00 +0900

    [Feat/#11] Input 컴포넌트 구현 (#12)

---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,36 @@
+import React, { HTMLAttributes } from "react";
+import { inputStyle, inputWrapper, labelStyle } from "./Input.style";
+
+export interface InputProps extends HTMLAttributes<HTMLInputElement> {
+  value: string;
+  placeholder?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type: string;
+  inputLabel?: string;
+}
+
+const Input = ({
+  value,
+  placeholder,
+  onChange,
+  type,
+  inputLabel,
+}: InputProps) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e);
+  };
+  return (
+    <div css={inputWrapper}>
+      {inputLabel && <span css={labelStyle}>{inputLabel}</span>}
+      <input
+        css={inputStyle}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+};
+
+export default Input;
