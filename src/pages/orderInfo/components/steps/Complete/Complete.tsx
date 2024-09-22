c9dedd9383b8e29dfda1c599d92bfac81c7f70e0
imhwarang <imhwarang4@naver.com>
2024-09-23 03:54:07 +0900

    feat: 주문 완료 페이지 퍼블리싱 완료

--- src/pages/orderInfo/components/steps/Complete/Complete.tsx
+++ src/pages/orderInfo/components/steps/Complete/Complete.tsx
@@ -1,11 +1,45 @@
-import { Header, ProgressBar } from "@components";
+import { Button, Header, ProgressBar } from "@components";
+import { IcComplete } from "@svg";
+import { useNavigate } from "react-router-dom";
+import {
+  iconStyle,
+  lastSpanWrapper,
+  layoutStyle,
+  spanContainer,
+  spanStyle,
+} from "./Complete.style";
+import { buttonSectionStyle } from "@pages/orderInfo/styles";
 
 const Complete = () => {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/");
+  };
   return (
-    <div>
+    <>
       <Header text="주문 완료" />
       <ProgressBar progress={100} />
-    </div>
+      <div css={layoutStyle}>
+        <IcComplete css={iconStyle} />
+        <div css={spanContainer}>
+          <span css={spanStyle}>주문이 완료되었어요</span>
+          <span css={spanStyle}>
+            주문번호: <span>{}</span>
+          </span>
+          <span css={[spanStyle, lastSpanWrapper]}>
+            카운터에서 주문번호로
+            <br />
+            결제해주세요
+          </span>
+        </div>
+        <footer css={buttonSectionStyle}>
+          <Button variant="fill" onClick={handleButtonClick}>
+            홈으로
+          </Button>
+        </footer>
+      </div>
+    </>
   );
 };
 
