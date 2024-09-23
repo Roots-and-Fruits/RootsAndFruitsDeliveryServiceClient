0987c5eff7149012333e6d22c354817c4606a87d
imhwarang <imhwarang4@naver.com>
2024-09-23 18:04:37 +0900

    feat: 빌드 후 수정

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,2 @@
 export * from "./routeType";
 export * from "./nextStep";
-export * from "./orderPostDataType";
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react({
+            jsxImportSource: '@emotion/react',
+        }),
+        tsconfigPaths(),
+        svgr({
+            svgrOptions: {
+                icon: true,
+                memo: true,
+            },
+        }),
+    ],
+});
