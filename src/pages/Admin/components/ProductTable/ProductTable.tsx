350d403a8647a455bf2efd58ebeec4cc4e8524fd
Yoo TaeSeung <remicon99@gmail.com>
2025-01-14 23:43:45 +0900

    feat: 관리자 상품 조회 순서 변경 기능 추가

--- src/pages/Admin/components/ProductTable/ProductTable.tsx
+++ src/pages/Admin/components/ProductTable/ProductTable.tsx
@@ -1,30 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   buttonContainer,
-  cellWidth,
-  checkboxStyle,
-  productNameStyle,
   sectionStyle,
   sectionTitle,
   tableStyle,
   tableTitle,
 } from "./ProductTable.style";
 import { Button, Modal } from "@components";
-import Switch from "react-switch";
 import { ProductWithSailed } from "@types";
 import { usePatchProduct } from "@apis/domains/admin/usePatchProduct";
 import { useDeleteProduct } from "@apis/domains/admin/useDeleteProduct";
 import ProductAddModal from "../ProductAddModal/ProductAddModal";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  arrayMove,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import SortableRow from "../SortableRow/SortableRow";
+import { usePatchSequence } from "@apis/domains/admin/usePatchSequence";
+
 interface ProductTableProps {
-  title: string;
+  title: "체험 상품" | "판매 상품";
   products: ProductWithSailed[];
 }
 
 const ProductTable = ({ title, products }: ProductTableProps) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedProducts, setSelectedProducts] = useState<number[]>([]);
+  const [productList, setProductList] = useState(products);
   const { mutate: patchProduct } = usePatchProduct();
   const { mutate: deleteProduct } = useDeleteProduct();
+  const { mutate: patchProductSequence } = usePatchSequence();
+
+  useEffect(() => {
+    setProductList(products);
+  }, [products]);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor)
+  );
 
   const handleModalClose = () => {
     setIsModalOpen(false);
@@ -40,17 +64,41 @@ const ProductTable = ({ title, products }: ProductTableProps) => {
 
   const handleSelectAllChange = (isChecked: boolean) => {
     if (isChecked) {
-      const allProductIds = products.map((product) => product.productId);
+      const allProductIds = productList.map((product) => product.productId);
       setSelectedProducts(allProductIds);
     } else {
       setSelectedProducts([]);
     }
   };
 
-  const isAllSelected = selectedProducts.length === products.length;
+  const isAllSelected = selectedProducts.length === productList.length;
 
   const handleDeleteClick = () => {
     deleteProduct(selectedProducts);
+  };
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (over && active.id !== over.id) {
+      const currentSequence = productList.findIndex(
+        (product) => product.productId === active.id
+      );
+      const newSequence = productList.findIndex(
+        (product) => product.productId === over.id
+      );
+      const productId = productList[currentSequence].productId;
+
+      setProductList((prevList) =>
+        arrayMove(prevList, currentSequence, newSequence)
+      );
+      patchProductSequence({
+        productId,
+        currentSequence:
+          title === "체험 상품" ? currentSequence : currentSequence + 1000,
+        newSequence: title === "체험 상품" ? newSequence : newSequence + 1000,
+      });
+    }
   };
 
   return (
@@ -66,50 +114,45 @@ const ProductTable = ({ title, products }: ProductTableProps) => {
           </Button>
         </div>
       </div>
-      <table css={tableStyle}>
-        <thead>
-          <tr>
-            <th>
-              <input
-                css={checkboxStyle}
-                type="checkbox"
-                checked={isAllSelected}
-                onChange={(e) => handleSelectAllChange(e.target.checked)}
-              />
-            </th>
-            <th>상품명</th>
-            <th>상품 가격</th>
-            <th>판매 중</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.productId}>
-              <td>
-                <input
-                  css={checkboxStyle}
-                  type="checkbox"
-                  checked={selectedProducts.includes(product.productId)}
-                  onChange={() => handleCheckboxChange(product.productId)}
+
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext
+          items={productList.map((product) => product.productId)}
+          strategy={verticalListSortingStrategy}
+        >
+          <table css={tableStyle}>
+            <thead>
+              <tr>
+                <th>
+                  <input
+                    type="checkbox"
+                    checked={isAllSelected}
+                    onChange={(e) => handleSelectAllChange(e.target.checked)}
+                  />
+                </th>
+                <th>상품명</th>
+                <th>상품 가격</th>
+                <th>판매 중</th>
+              </tr>
+            </thead>
+            <tbody>
+              {productList.map((product) => (
+                <SortableRow
+                  key={product.productId}
+                  product={product}
+                  selectedProducts={selectedProducts}
+                  handleCheckboxChange={handleCheckboxChange}
+                  patchProduct={patchProduct}
                 />
-              </td>
-              <td css={productNameStyle}>{product.productName}</td>
-              <td css={cellWidth(15)}>
-                {`${product.productPrice.toLocaleString()}원`}
-              </td>
-              <td css={cellWidth(15)}>
-                <Switch
-                  checked={product.isSailed}
-                  onChange={() => patchProduct(product.productId)}
-                  uncheckedIcon={false}
-                  checkedIcon={false}
-                  onColor="#EC6732"
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              ))}
+            </tbody>
+          </table>
+        </SortableContext>
+      </DndContext>
 
       {isModalOpen && (
         <Modal onClose={handleModalClose}>
